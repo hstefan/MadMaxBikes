26d0e685b127e2a35b0f3953217e85f6cc88c61c
Yuri Kunde Schlesner <yuriks@yuriks.net>
2014-08-02 18:59:46 -0300

    Optimize to 1000fps

--- testst.lua
+++ testst.lua
@@ -2,20 +2,19 @@ local level = require 'level'
 
 local testst = {}
 
-local ww, wh = love.graphics.getDimensions()
+local win_w, win_h = love.graphics.getDimensions()
 
 function testst:init()
 	level:load_map('map_placeholder.lua')
 	self.world = level.world
 	self.object = { }
-	self.object.body = love.physics.newBody(self.world, ww/2, 30, "dynamic")
+	self.object.body = love.physics.newBody(self.world, win_w/2, 30, "dynamic")
 	self.object.shape = love.physics.newRectangleShape(30, 60)
 	self.object.fixture = love.physics.newFixture(self.object.body, self.object.shape)
 	self.object.fixture:setRestitution(0.3)
 end
 
 function testst:draw()
-	win_w, win_h = love.graphics.getDimensions()
 	scroll_x, scroll_y = self.object.body:getPosition()
 	scroll_x = scroll_x - win_w / 2
 	scroll_y = scroll_y - win_h / 2
