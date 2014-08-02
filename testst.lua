e69ca605a791cf25b205bee8102c35a93d0baa57
Yuri Kunde Schlesner <yuriks@yuriks.net>
2014-08-02 19:33:57 -0300

    TWO-PLAYER ACTION

--- testst.lua
+++ testst.lua
@@ -4,40 +4,69 @@ local testst = {}
 
 local win_w, win_h = love.graphics.getDimensions()
 
+function testst:create_player(p, x, y)
+	local object = {}
+	if p == 1 then
+		object.color = { 255, 255, 40 }
+		object.keys = { left = 'left', right = 'right', up = 'up' }
+	else
+		object.color = { 255, 40, 255 }
+		object.keys = { left = 'a', right = 's', up = 'w' }
+	end
+	object.body = love.physics.newBody(level.world, x, y, "dynamic")
+	object.shape = love.physics.newRectangleShape(30, 60)
+	object.fixture = love.physics.newFixture(object.body, object.shape)
+	object.fixture:setRestitution(0.3)
+
+	function object:update(dt)
+		if love.keyboard.isDown(self.keys.left) then
+			self.body:applyLinearImpulse(-15, 0)
+		end
+		if love.keyboard.isDown(self.keys.right) then
+			self.body:applyLinearImpulse(15, 0)
+		end
+		if love.keyboard.isDown(self.keys.up) then
+			self.body:applyLinearImpulse(0, -10)
+		end
+	end
+
+	function object:draw()
+		love.graphics.setColor(self.color)
+		love.graphics.polygon('fill', self.body:getWorldPoints(self.shape:getPoints()))
+	end
+
+	return object
+end
+
 function testst:init()
 	level:load_map('map_placeholder.lua')
-	self.world = level.world
-	self.object = { }
-	self.object.body = love.physics.newBody(self.world, win_w/2, 30, "dynamic")
-	self.object.shape = love.physics.newRectangleShape(30, 60)
-	self.object.fixture = love.physics.newFixture(self.object.body, self.object.shape)
-	self.object.fixture:setRestitution(0.3)
+	self.p1 = self:create_player(1, win_w/3, 30)
+	self.p2 = self:create_player(2, win_w/3*2, 30)
 end
 
 function testst:draw()
-	scroll_x, scroll_y = self.object.body:getPosition()
-	scroll_x = scroll_x - win_w / 2
-	scroll_y = scroll_y - win_h / 2
+	p1_x, p1_y = self.p1.body:getPosition()
+	p2_x, p2_y = self.p2.body:getPosition()
+	scroll_x = (p1_x + p2_x) / 2
+	scroll_y = (p1_y + p2_y) / 2
+	dist_x = (math.abs(p1_x - p2_x)) * 1.25
+	dist_y = (math.abs(p1_y - p2_y)) * 1.25
+	distance = math.max(dist_x, dist_y, 384) / math.min(win_w, win_h)
 	love.graphics.origin()
+	love.graphics.translate(win_w / 2, win_h / 2)
+	love.graphics.scale(1 / distance)
 	love.graphics.translate(-scroll_x, -scroll_y)
 
-	love.graphics.setColor(255, 255, 40)
-	love.graphics.polygon('fill', self.object.body:getWorldPoints(self.object.shape:getPoints()))
+	self.p1:draw()
+	self.p2:draw()
 	love.graphics.setColor(255, 255, 255)
 	level:draw_layer(level.layers['Objects'])
 end
 
 function testst:update(dt)
-	self.world:update(dt)
-	if love.keyboard.isDown('left') then
-		self.object.body:applyLinearImpulse(-15, 0)
-	end
-	if love.keyboard.isDown('right') then
-		self.object.body:applyLinearImpulse(15, 0)
-	end
-	if love.keyboard.isDown('up') then
-		self.object.body:applyLinearImpulse(0, -10)
-	end
+	level.world:update(dt)
+	self.p1:update(dt)
+	self.p2:update(dt)
 end
 
 return testst
