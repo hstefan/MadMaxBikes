2a0e4f642cddc8a8f6ae6696278856e3c0bc7157
Yuri Kunde Schlesner <yuriks@yuriks.net>
2014-08-02 05:33:38 -0300

    Add rotation support

--- level.lua
+++ level.lua
@@ -4,7 +4,7 @@ function mod:load_tileset(tileset)
 	local gid = tileset.firstgid
 
 	if tileset.image ~= nil then
-		local image = love.graphics.newImage(tileset.image)
+		local image = love.graphics.newImage('data/' .. tileset.image)
 		local x = 0
 		local y = 0
 		while y + tileset.tileheight <= tileset.imageheight do
@@ -12,7 +12,9 @@ function mod:load_tileset(tileset)
 				self.tiles[gid] = {
 					quad = love.graphics.newQuad(x, y, tileset.tilewidth, tileset.tileheight,
 						tileset.imagewidth, tileset.imageheight),
-					image = image
+					image = image,
+					origin_x = 0,
+					origin_y = tileset.tileheight,
 				}
 				gid = gid + 1
 				x = x + tileset.tilewidth
@@ -22,19 +24,46 @@ function mod:load_tileset(tileset)
 		end
 	else
 		for i, tile in ipairs(tileset.tiles) do
-			local image = love.graphics.newImage(tile.image)
+			local image = love.graphics.newImage('data/' .. tile.image)
 			self.tiles[gid + tile.id] = {
 				quad = love.graphics.newQuad(0, 0, tile.width, tile.height, tile.width, tile.height),
-				image = image
+				image = image,
+				origin_x = 0,
+				origin_y = tile.height,
 			}
 		end
 	end
 end
 
+--[[
+function convert_gid(gid)
+	local hflip_mask = 0x80000000
+	local vflip_mask = 0x40000000
+	local hflip = 1
+	local vflip = 1
+	if gid >= hflip_mask then
+		hflip = -1
+		gid = gid - hflip_mask
+	end
+	if gid >= vflip_mask then
+		vflip = -1
+		gid = gid - vflip_mask
+	end
+	return gid, hflip, vflip
+end
+--]]
+
+function mod:draw_object(object)
+	local tile = self.tiles[object.gid]
+	local angle = object.rotation * math.pi / 180
+	love.graphics.draw(tile.image, tile.quad,
+		object.x, object.y,
+		angle, 1, 1, tile.origin_x, tile.origin_y)
+end
+
 function mod:draw_layer(layer)
 	for i, object in ipairs(layer.objects) do
-		local tile = self.tiles[object.gid]
-		love.graphics.draw(tile.image, tile.quad, object.x, object.y)
+		self:draw_object(object)
 	end
 end
 
