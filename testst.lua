d97accdaaf464ad2050eba66adbf3579af50cc8a
Hugo Puhlmann <hugopuhlmann@gmail.com>
2014-08-02 18:13:50 -0300

    Adding level-world hooks to testst

--- testst.lua
+++ testst.lua
@@ -5,16 +5,8 @@ local testst = {}
 local ww, wh = love.graphics.getDimensions()
 
 function testst:init()
-	level:load_map('test_map.lua')
-
-	love.physics.setMeter(64)
-	self.world = love.physics.newWorld(0, 9.81 * 64, true)
-
-	self.ground = { }
-	self.ground.body = love.physics.newBody(self.world, ww/2, wh - 30, "static")
-	self.ground.shape = love.physics.newRectangleShape(ww, 60)
-	self.ground.fixture = love.physics.newFixture(self.ground.body, self.ground.shape)
-
+	level:load_map('map_placeholder.lua')
+	self.world = level.world
 	self.object = { }
 	self.object.body = love.physics.newBody(self.world, ww/2, 30, "dynamic")
 	self.object.shape = love.physics.newRectangleShape(30, 60)
@@ -23,18 +15,14 @@ function testst:init()
 end
 
 function testst:draw()
-	love.graphics.setColor(255, 255, 255)
-	level:draw_layer(level.layers['map_images'])
-
-	love.graphics.setColor(255, 120, 40)
-	love.graphics.polygon('fill', self.ground.body:getWorldPoints(self.ground.shape:getPoints()))
 	love.graphics.setColor(255, 255, 40)
 	love.graphics.polygon('fill', self.object.body:getWorldPoints(self.object.shape:getPoints()))
+	love.graphics.setColor(255, 255, 255)
+	level:draw_layer(level.layers['Objects'])
 end
 
 function testst:update(dt)
 	self.world:update(dt)
-
 	if love.keyboard.isDown('up') then
 		self.object.body:applyLinearImpulse(0, -60)
 	end
